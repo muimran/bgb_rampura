8a3e73518a7da79caf8ea01165d8eb8261b80752
Muhammad Imran <imran271828@gmail.com>
2025-08-07 05:21:38 +0600

    BUILD FOR DRUPAL 7?

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+// Import the static adapter
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		// Use the static adapter
+		adapter: adapter({
+			// default options are fine
+		}),
+
+		// This is the crucial part for the subfolder
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/bgb' : ''
+		}
+	}
+};
 
 export default config;
